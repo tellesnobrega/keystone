66979b089ab854e0affcc8ff70c8cc12e2b1b365
Raildo Mascena <raildo@lsd.ufcg.edu.br>
2014-02-13 15:24:40 -0300

    root project

--- keystone/assignment/backends/sql.py
+++ keystone/assignment/backends/sql.py
@@ -408,17 +408,20 @@ class Assignment(sql.Base, assignment.Driver):
     # CRUD
     @sql.handle_conflicts(conflict_type='project')
     def create_project(self, tenant_id, tenant):
+        default_name = 'openstack'
         tenant['name'] = clean.project_name(tenant['name'])
         with sql.transaction() as session:
             tenant_ref = Project.from_dict(tenant)
-	    temp_name = ''
-	    if tenant['parent_project_id'] is not None:
-	        parent_tenant = self._get_project(session,
+        temp_name = ''
+        if tenant['parent_project_id'] is not None:
+            parent_tenant = self._get_project(session,
 	                                          tenant['parent_project_id'])
-	        temp_name = parent_tenant['name'] + '.' + tenant['name']
-	        tenant_ref.name = temp_name
-	    session.add(tenant_ref)
-	    return tenant_ref.to_dict()
+            temp_name = parent_tenant['name'] + '.' + tenant['name']
+            tenant_ref.name = temp_name
+        else:
+            tenant_ref.name = default_name
+        session.add(tenant_ref)
+        return tenant_ref.to_dict()
 
     @sql.handle_conflicts(conflict_type='project')
     def update_project(self, tenant_id, tenant):
@@ -684,8 +687,7 @@ class Project(sql.ModelBase, sql.DictBase):
                   'description', 'enabled']
     id = sql.Column(sql.String(64), primary_key=True)
     name = sql.Column(sql.String(64), nullable=False)
-    domain_id = sql.Column(sql.String(64), sql.ForeignKey('domain.id'),
-                           nullable=False)
+    domain_id = sql.Column(sql.String(64), nullable=False)
     parent_project_id = sql.Column(sql.String(64))
     description = sql.Column(sql.Text())
     enabled = sql.Column(sql.Boolean)
