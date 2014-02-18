c5466f2096ab651857609fc0b75e16acfc0b34d3
tellesnobrega <tellesnobrega@gmail.com>
2014-02-18 10:52:45 -0300

    Hierarchical Ids - test sql

--- keystone/assignment/backends/sql.py
+++ keystone/assignment/backends/sql.py
@@ -53,10 +53,14 @@ class Assignment(sql.Base, assignment.Driver):
                 raise exception.ProjectNotFound(project_id=tenant_name)
             return project_ref.to_dict()
 
-    def get_project_hierarchy(self, tenant_id):
+    def get_project_test(self, tenant_id):
+        with sql.transaction() as session:
+            return self._get_project(session, tenant_id).to_dict()
+
+    '''def get_project_hierarchy(self, tenant_id):
         with sql.transaction() as session:
             return  self._get_project(session, tenant_id).to_dict()
-	        '''tenant = tenant_ref.to_dict()
+	        tenant = tenant_ref.to_dict()
             hierarchy = tenant['id']
             while tenant['parent_project_id'] is not None:
                 parent_tenant_ref = self._get_project(session,
